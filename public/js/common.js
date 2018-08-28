e01031ef30d3b602f35d8f6e7196e08dc6fd1f50
Schuyler Ankele <schuyler.ankele@charter.com>
2018-08-27 20:14:33 -0600

    reload page on sub

--- public/js/common.js
+++ public/js/common.js
@@ -8,9 +8,12 @@ $(document).ready(function (){
 $("#submit").on('click', function (e){
     e.preventDefault();
     let text = $('#burrito').val().trim();
+    $('#burrito').text("");
     $.post("/api/add", {text:text}, function (data){
         console.log(data);
         listBurritos(data);
+    }).then(function(){
+        location.reload();
     });
 });
 
